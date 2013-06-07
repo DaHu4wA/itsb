0e3f351f2b510247b26392736157562d556f83c1
dahu4wa@gmail.com <dahu4wa@gmail.com@9369d5c0-7047-9487-1be3-f8e57e06abaf>
2013-06-07 17:14:47 +0000

    initial

---
+++ trunk/MMTGull_WebSiteSemantic/src/ac/at/fhsalzburg/semantic/data/WordsWithCountAndUrl.java
@@ -0,0 +1,27 @@
+package ac.at.fhsalzburg.semantic.data;
+
+import java.util.Map;
+
+public class WordsWithCountAndUrl {
+	
+	final String rssFeedUrl;
+	final Map<String, Integer> wordsWithCount;
+
+	public WordsWithCountAndUrl(String rssFeedUrl, Map<String, Integer> wordsWithCount){
+		this.rssFeedUrl = rssFeedUrl;
+		this.wordsWithCount = wordsWithCount;
+	}
+
+	public String getRssFeedUrl() {
+		return rssFeedUrl;
+	}
+
+	public Map<String, Integer> getWordsWithCount() {
+		return wordsWithCount;
+	}
+	
+	@Override
+	public String toString(){
+		return "RSS \""+rssFeedUrl + "\" (contains "+wordsWithCount.size()+" different words)";
+	}
+}
